ecf40c6271e4c8e8e00cc165a410049fe42741fe
Vikash Tiwari <vikashvt66793@gmail.com>
2026-04-25 15:13:29 +0530

    Update app.js

--- Minor_Backend/app.js
+++ Minor_Backend/app.js
@@ -10,9 +10,11 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(cors({
-  origin: "https://visionarys.netlify.app", // your frontend URL
+  origin: ["http://localhost:5500", "http://127.0.0.1:5500", "https://visionarys.netlify.app"], // your frontend URLs
   credentials: true
 }));
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,14 +22,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userController);
 app.use("/api/posts", postController);
 
-const pages = ["", "about", "contact", "sign-up", "sign-in", "dashboard", "profile", "user-profile"];
-pages.forEach(p => {
-  const route = p === "" ? "/" : `/${p}`;
-  const file = p === "" ? "index" : p;
-  app.get(route, (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', `${file}.html`));
-  });
-});
+// const pages = ["", "about", "contact", "sign-up", "sign-in", "dashboard", "profile", "user-profile"];
+// pages.forEach(p => {
+//   const route = p === "" ? "/" : `/${p}`;
+//   const file = p === "" ? "index" : p;
+//   app.get(route, (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', `${file}.html`));
+//   });
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
